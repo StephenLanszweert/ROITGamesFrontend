534c26be46ad885abe6233e3fc580e627d1b818c
Robin Staelens <robin.staelens@student.howest.be>
2018-12-02 19:50:25 +0100

    Added usage card to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {GraphsComponent} from './components/graphs/graphs.component';
 import {UsageSummaryComponent} from './components/usage-summary/usage-summary.component';
+import {UsageCardComponent} from './components/usage-card/usage-card.component';
 
 import {WaterwellService} from './services/waterwell.service';
 
-
 @NgModule({
-    declarations: [AppComponent, HomeComponent, GraphsComponent, UsageSummaryComponent],
+    declarations: [AppComponent, HomeComponent, GraphsComponent, UsageSummaryComponent, UsageCardComponent],
     imports: [BrowserModule, HttpClientModule],
     providers: [WaterwellService],
     bootstrap: [AppComponent]
