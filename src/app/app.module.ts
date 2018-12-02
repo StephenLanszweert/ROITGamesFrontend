595f969cb6f586aecd2eba1d4fe49ef2a45dd6ef
Robin Staelens <robin.staelens@student.howest.be>
2018-12-02 19:49:17 +0100

    Cleaned import formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {GraphsComponent} from './components/graphs/graphs.component';
 import {UsageSummaryComponent} from './components/usage-summary/usage-summary.component';
+
 import {WaterwellService} from './services/waterwell.service';
-import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, GraphsComponent, UsageSummaryComponent],
